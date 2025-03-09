492ae1a237ab87f127e7110047f6e36a60731a30
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-03-09 13:23:30 +0000

    styles color

---
+++ app/playground/page.tsx
@@ -0,0 +1,9 @@
+export default function Playground(){
+    return (
+        <main className="flex w-screen h-screen">
+            <nav className="w-full bg-primary h-40">
+                
+            </nav>
+        </main>
+    )
+}
