938f20afa8ebcfd856bda336940a0c8c58b10e93
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-03-09 14:59:47 +0000

    Removed the unused import

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,10 +1,9 @@
-import Link from "next/link";
 import Footer from "@/app/components/Footer";
 import Nav from "@/app/components/Nav";
 export default function Playground() {
   return (
     <main className="flex flex-col w-screen h-screen">
-      <Nav/>
+      <Nav />
       <div className="h-96"></div>
       <Footer />
     </main>
