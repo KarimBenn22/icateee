6a791ec8c8f37159bc87cfef7e68de85cc052496
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-03-09 14:45:39 +0000

    make nav a standalone component

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,50 +1,10 @@
 import Link from "next/link";
-import Footer from "../components/footer/Footer";
-// Todo: REFACTOR THIS LATER
+import Footer from "@/app/components/Footer";
+import Nav from "@/app/components/Nav";
 export default function Playground() {
   return (
     <main className="flex flex-col w-screen h-screen">
-      <nav className="w-full bg-primary h-40 font-semibold text-medium tracking-wider">
-        <div className="w-full h-25"></div>
-        <ul className="flex gap-4 w-full justify-center">
-          <li className="p-2 relative group">
-            <Link href={""} className="relative inline-block">
-              HOME
-              <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
-            </Link>
-          </li>
-          <li className="p-2 relative group">
-            <Link href={""} className="relative inline-block">
-              COMMITEES
-              <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
-            </Link>
-          </li>
-          <li className="p-2 relative group">
-            <Link href={""} className="relative inline-block">
-              SUBMISSION
-              <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
-            </Link>
-          </li>
-          <li className="p-2 relative group">
-            <Link href={""} className="relative inline-block">
-              SPEAKERS
-              <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
-            </Link>
-          </li>
-          <li className="p-2 relative group">
-            <Link href={""} className="relative inline-block">
-              CALL FOR PAPERS
-              <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
-            </Link>
-          </li>
-          <li className="p-2 relative group">
-            <Link href={""} className="relative inline-block">
-              DISCOVER
-              <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Nav/>
       <div className="h-96"></div>
       <Footer />
     </main>
