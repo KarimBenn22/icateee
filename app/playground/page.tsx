b4f01cc9be34e5d17c25036c065ccbb3fba381a2
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-03-12 15:08:18 +0000

    css only 2 image carousel

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -3,15 +3,14 @@ import Nav from "@/app/components/Nav";
 import HeaderSection from "@/app/components/sections/Header";
 import Commitee from "../components/sections/Commitee";
 import Chairs from "../components/sections/Chairs";
+import CarouselPlayground from "../components/sections/HeaderCarousel";
 
 export default function Playground() {
   return (
     <main className="flex flex-col items-center">
       <Nav />
-      <div className="max-w-[1200px]">
-        <HeaderSection></HeaderSection>
-        <Chairs></Chairs>
-        <Commitee></Commitee>
+      <div className="">
+        <CarouselPlayground></CarouselPlayground>
       </div>
       <Footer />
     </main>
