cfbad809ed945f550f4c7f1aeb1e02fca8dd721b
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-03-14 23:27:11 +0000

    cleanup committees route

--- app/commitees/page.tsx
+++ app/commitees/page.tsx
@@ -1,16 +1,12 @@
 import ChairsSection from "@/app/components/sections/Chairs";
 import CommitteeSection from "@/app/components/sections/Commitee";
-import Nav from "../components/Nav";
-import Footer from "../components/Footer";
 import ConferenceParticipants from "../components/sections/ConferenceParticipants";
 export default function CommiteesPage() {
   return (
     <main className="flex flex-col items-center">
-      <Nav />
       <ChairsSection></ChairsSection>
       <CommitteeSection></CommitteeSection>
       <ConferenceParticipants />
-      <Footer />
     </main>
   );
 }
