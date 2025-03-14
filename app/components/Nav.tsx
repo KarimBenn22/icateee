2d4315419af997b0e6c46b3a59616119a3af4aa1
Islam Zaoui <98623858+IslamZaoui@users.noreply.github.com>
2025-03-14 15:00:08 +0100

    fix nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="w-full bg-primary p-4 fixed top-0 z-200">
+      <nav className="w-full bg-primary p-4 sticky top-0 z-999">
         <div className="flex items-center justify-center relative">
           <div
             className={`absolute left-1/2 transform -translate-x-1/2 transition-all duration-500 ${
