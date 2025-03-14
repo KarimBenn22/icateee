3f22deb18fb1ba780363b7a39d0d5cca4a8aac23
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-03-14 22:51:06 +0000

    speakers basic component

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -6,6 +6,7 @@ import ImportantDatesSection from "../components/sections/ImportantDates";
 import ConferenceParticipants from "../components/sections/ConferenceParticipants";
 import Topics from "../components/sections/topics";
 import { SponsorsSection } from "../components/sections/sponsors";
+import { SpeakersSection } from "../components/sections/speakers";
 
 export default function Playground() {
   return (
@@ -17,6 +18,7 @@ export default function Playground() {
         <ImportantDatesSection />
         <RegistrationSection />
         <SponsorsSection />
+        <SpeakersSection></SpeakersSection>
       </div>
       <Footer />
     </main>
