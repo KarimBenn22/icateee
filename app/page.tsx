18ab5f9cc6993144e7955689eee56e9fff2d64ae
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-03-14 23:31:06 +0000

    correct home page, and remove playground route

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import Nav from "@/app/components/Nav";
 import HeaderSection from "@/app/components/sections/Header";
 import Footer from "@/app/components/Footer";
-
+import Topics from "./components/sections/topics";
+import ImportantDatesSection from "./components/sections/ImportantDates";
+import RegistrationSection from "./components/sections/Registration";
+import { SponsorsSection } from "./components/sections/sponsors";
 export default function Home() {
   return (
-      <main className="flex flex-col items-center">
-        <Nav />
-        <div className="max-w-[1200px]">
-        </div>
-        <Footer />
-      </main>
+    <main className="flex flex-col items-center">
+      <Nav />
+      <HeaderSection />
+      <div className="max-w-6xl">
+        <Topics />
+        <ImportantDatesSection />
+        <RegistrationSection />
+        <SponsorsSection />
+      </div>
+      <Footer />
+    </main>
   );
 }
